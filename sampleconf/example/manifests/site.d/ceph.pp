087ec3a48bd2d64deb448405663cbad2abecaa79
root <root@zw-puppetmaster.(none)>
2013-12-17 14:28:01 +0800

    modified sampleconf/example/manifests/site.d/ceph.pp

--- sampleconf/example/manifests/site.d/ceph.pp
+++ sampleconf/example/manifests/site.d/ceph.pp
@@ -1,7 +1,7 @@
 $ceph_conf = {
-    fsid =>'a316d7d6-2b18-446c-9a91-0fa36a2dc1e3',
-    mon_initial_members => ['xm1-ceph-mon1','xm1-ceph-mon2','xm1-ceph-mon3'],
-    mon_host => ['10.100.40.11','10.100.40.12','10.100.40.13'],
-    public_network => '10.100.40.0/24',
-    cluster_network => '10.100.41.0/24',
+    fsid => 'a1fadefe-6068-4d10-8c53-4a864beab049',
+    mon_initial_members => ['ceph1','ceph2','ceph3'],
+    mon_host => ['10.1.1.60','10.1.1.61','10.1.1.62'],
+    public_network => '10.1.1.0/24',
+    cluster_network => '10.1.2.0/24',
 }
