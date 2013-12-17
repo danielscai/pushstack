4c32f374e64ab12bdb0e15f7c5a046e1afd93752
root <root@zw-puppetmaster.(none)>
2013-12-17 17:53:20 +0800

    modified: ceph/manifests/repo.pp for adding ceph.list

--- ceph/manifests/repo.pp
+++ ceph/manifests/repo.pp
@@ -1,8 +1,20 @@
-class ceph::repo (
-  $release = 'dumpling'
-) {
-  apt::key { 'ceph':
-    key        => '17ED316D',
-    key_source => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
-  }
+class ceph::repo {
+    case $operatingsystem {
+
+        /(Ubuntu|Debian)/: {
+            apt::key { 'ceph':
+                key        => '17ED316D',
+                key_source => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
+            }    
+           
+            apt::source { 'ceph':
+                location    => 'http://ceph.com/debian-emperor/',
+                release     => "$::lsbdistcodename",
+                repos       => 'main',
+                include_src => false,
+                #key         => '17ED316D',
+                #key_content => template("$module_name/rabbit.asc.erb"),
+            }
+        } 
+    }   
 }
