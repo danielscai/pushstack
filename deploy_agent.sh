744c9b3dc01513079121da9135477d8260e35d37
root <root@zw-puppetmaster.(none)>
2013-12-23 13:41:04 +0800

    add global variable:apt_ubuntu_url,for ubuntu repo management

--- deploy_agent.sh
+++ deploy_agent.sh
@@ -2,6 +2,4 @@
 wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb -O puppetlabs-release-precise.deb
 dpkg -i puppetlabs-release-precise.deb
 apt-get update
-apt-get install puppetmaster puppet -y
-
-cp -fr /etc/puppet/modules/sampleconf/example/* /etc/puppet/
+apt-get install puppet -y
--- openstack/manifests/repo/rhel.pp
+++ openstack/manifests/repo/rhel.pp
@@ -2,7 +2,7 @@ class openstack::repo::apt {
 
     #Class['openstack-grizzly::repo'] -> Class['openstack-grizzly::controller']
     apt::source { 'grizzly':
-        location    => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+        location    => "http://ubuntu-cloud.archive.canonical.com/ubuntu",
         release     => 'precise-updates/grizzly',
         repos       => 'main',
         include_src => false,
